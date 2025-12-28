b5fe5c11da59df204ff8b7fd81ca8d7744d25163
Daylam Tayari <git@tayari.gg>
2025-12-28 11:21:38 -0600

    Added api key to user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -10,6 +10,7 @@ type User struct {
 	ID           uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
 	Email        string    `gorm:"type:varchar(255);not null;uniqueIndex"`
 	PasswordHash *string   `gorm:"type:varchar(255)"`
+	ApiKey       *string   `gorm:"type:varchar(255)"`
 
 	// User management
 	IsAdmin             bool       `gorm:"not null;default:false"`
