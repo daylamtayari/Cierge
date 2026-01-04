04a2ae38ed3d31dd4feb8689d7c1d3c647554bcf
Daylam Tayari <git@tayari.gg>
2026-01-04 16:09:47 -0600

    Added user ID to the context

--- internal/context/context.go
+++ internal/context/context.go
@@ -13,7 +13,7 @@ type ctxKey string
 const (
 	loggerKey    ctxKey = "logger"
 	requestIDKey ctxKey = "request_id"
-	claimsKey    ctxKey = "claims"
+	userIDKey    ctxKey = "user_id"
 )
 
 // Adds a logger to the context
@@ -48,4 +48,21 @@ func RequestID(ctx context.Context) string {
 	return fallbackID
 }
 
-// TODO: Implement claims
+// Adds a user ID to the context
+func WithUserID(ctx context.Context, userID uuid.UUID) context.Context {
+	return context.WithValue(ctx, userIDKey, userID)
+}
+
+// Returns the user ID from the context
+func UserID(ctx context.Context) uuid.UUID {
+	if userID, ok := ctx.Value(userIDKey).(uuid.UUID); ok {
+		return userID
+	}
+	// If the value was not set, return a zero value
+	// This *should* never happen, but this zero value
+	// UUID makes sure that any future user operations
+	// should fail as there can never be a corresponding
+	// user ID. A UUIDv4 can never be all zeros due to
+	// the version and variant bits.
+	return uuid.UUID{}
+}
