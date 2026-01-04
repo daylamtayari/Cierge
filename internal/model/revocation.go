6feca39218f8e828fbd03b727bda023d36c77c3f
Daylam Tayari <git@tayari.gg>
2026-01-03 20:59:17 -0600

    Created revocation model

---
+++ internal/model/revocation.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Revocation struct {
+	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null;index"`
+	JTI       string    `gorm:"column:jti;type:varchar(255);not null;uniqueIndex"`
+	RevokedAt time.Time `gorm:"not null;default:now()"`
+}
