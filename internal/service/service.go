eed68b0ed0f66b02fe0e81820f5e9639eb2d380e
Daylam Tayari <git@tayari.gg>
2026-01-08 09:50:17 -0600

    Added auth service to services

--- internal/service/service.go
+++ internal/service/service.go
@@ -9,14 +9,17 @@ type Services struct {
 	Token  *TokenService
 	User   *UserService
 	Health *HealthService
+	Auth   *AuthService
 }
 
 func New(repos *repository.Repositories, cfg *config.Config) *Services {
 	userService := NewUserService(repos.User)
+	tokenService := NewTokenService(userService, cfg.Auth)
 
 	return &Services{
 		User:   userService,
-		Token:  NewTokenService(userService, cfg.Auth),
+		Token:  tokenService,
 		Health: NewHealthService(repos.DB(), repos.Timeout()),
+		Auth:   NewAuthService(userService, tokenService, &cfg.Auth),
 	}
 }
