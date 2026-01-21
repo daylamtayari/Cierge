05579c0122a5227b85cb4e74b4b16d03eec96371
Daylam Tayari <git@tayari.gg>
2026-01-21 17:21:37 -0600

    Added creation of BookingClient

--- lambdas/reservation/book.go
+++ lambdas/reservation/book.go
@@ -1,6 +1,13 @@
 package main
 
-import "context"
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrUnsupportedPlatform = errors.New("unsupported platform")
+)
 
 type BookingClient interface {
 	// Handles any pre-booking checks such as token validaty
@@ -8,4 +15,15 @@ type BookingClient interface {
 
 	// Attempts to perform a booking
 	Book(ctx context.Context, event LambdaEvent) (*BookingResult, error)
+}
+
+// Returns a new booking client for the specified platform
+// NOTE: Add OpenTable client when created
+func NewBookingClient(platform string, token string) (BookingClient, error) {
+	switch platform {
+	case "resy":
+		return NewResyClient(token)
+	default:
+		return nil, ErrUnsupportedPlatform
+	}
 }
