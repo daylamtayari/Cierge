3fef6e435e3b6c2193a88794f041fd155a4c55b9
Daylam Tayari <git@tayari.gg>
2026-01-21 07:21:54 -0600

    Created reservation lambda main

---
+++ lambdas/reservation/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/kms"
+)
+
+var kmsClient *kms.Client
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		panic("failed to load aws config: " + err.Error())
+	}
+
+	kmsClient = kms.NewFromConfig(cfg)
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
