e6805eb82f3097e26927caddf12a6256cf6e72bc
Daylam Tayari <git@tayari.gg>
2026-02-03 11:26:24 -0600

    Added methods to the reservation repository

--- server/internal/model/reservation.go
+++ server/internal/model/reservation.go
@@ -12,10 +12,10 @@ type Reservation struct {
 	JobID        *uuid.UUID `gorm:"type:uuid"`
 	RestaurantID uuid.UUID  `gorm:"type:uuid;not null;index:idx_reservations_restaurant"`
 
-	Platform         Platform `gorm:"type:varchar(50);not null"`
-	ConfirmationCode *string  `gorm:"type:varchar(255)"`
-	ReservationAt    string   `gorm:"type:timestamptz;not null;index:idx_reservation_user_at"`
-	PartySize        int16    `gorm:"type:smallint;not null"`
+	Platform      Platform `gorm:"type:varchar(50);not null"`
+	Confirmation  *string  `gorm:"type:varchar(255)"`
+	ReservationAt string   `gorm:"type:timestamptz;not null;index:idx_reservation_user_at"`
+	PartySize     int16    `gorm:"type:smallint;not null"`
 
 	// Relations
 	User       *User       `gorm:"foreignKey:UserID"`
--- server/internal/repository/reservation.go
+++ server/internal/repository/reservation.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"context"
 	"time"
 
+	"github.com/daylamtayari/cierge/server/internal/model"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -16,4 +19,64 @@ func NewReservationRepository(db *gorm.DB, timeout time.Duration) *ReservationRe
 		db:      db,
 		timeout: timeout,
 	}
+}
+
+// Gets a reservation from a given ID
+func (r *ReservationRepository) GetByID(ctx context.Context, id uuid.UUID) (*model.Reservation, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	var reservation model.Reservation
+	if err := r.db.WithContext(ctx).Where("id = ?", id).First(&reservation).Error; err != nil {
+		return nil, err
+	}
+	return &reservation, nil
+}
+
+// Gets all reservations for a given user
+func (r *ReservationRepository) GetByUser(ctx context.Context, userID uuid.UUID) ([]*model.Reservation, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	var reservations []*model.Reservation
+	if err := r.db.WithContext(ctx).Where("user_id = ?", userID).Find(&reservations).Error; err != nil {
+		return nil, err
+	}
+	return reservations, nil
+}
+
+// Get all upcoming reservations for a given user
+func (r *ReservationRepository) GetByUserUpcoming(ctx context.Context, userID uuid.UUID) ([]*model.Reservation, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	var reservations []*model.Reservation
+	if err := r.db.WithContext(ctx).Where("user_id = ? AND reservation_at > ?", userID, time.Now().UTC()).Find(&reservations).Error; err != nil {
+		return nil, err
+	}
+	return reservations, nil
+}
+
+// Create a reservation
+func (r *ReservationRepository) Create(ctx context.Context, reservation *model.Reservation) error {
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	return r.db.WithContext(ctx).Create(reservation).Error
+}
+
+// Update a reservation
+func (r *ReservationRepository) Update(ctx context.Context, reservation *model.Reservation) error {
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	return r.db.WithContext(ctx).Save(reservation).Error
+}
+
+// Delete a reservation
+func (r *ReservationRepository) Delete(ctx context.Context, id uuid.UUID) error {
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	return r.db.WithContext(ctx).Delete(&model.Reservation{}, "id = ?", id).Error
 }
