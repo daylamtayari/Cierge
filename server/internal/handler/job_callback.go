d1e89d59bc74c35dd87836eac9a674e3d1dccd4f
Daylam Tayari <git@tayari.gg>
2026-02-03 09:34:22 -0600

    Created job callback handler

--- server/internal/handler/job_callback.go
+++ server/internal/handler/job_callback.go
@@ -1,8 +1,14 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/daylamtayari/cierge/reservation"
+	appctx "github.com/daylamtayari/cierge/server/internal/context"
+	"github.com/daylamtayari/cierge/server/internal/model"
 	"github.com/daylamtayari/cierge/server/internal/service"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 )
 
 type JobCallbackHandler struct {
@@ -15,6 +21,47 @@ func NewJobCallbackHandler(jobService *service.JobService) *JobCallbackHandler {
 	}
 }
 
+// Handles a callback request from a job output and updates the
+// job value, creates a reservation, and sends a notification
 func (h *JobCallbackHandler) HandleJobCallback(c *gin.Context) {
+	errorCol := appctx.ErrorCollector(c.Request.Context())
 
+	var callbackReq reservation.Output
+	if err := c.ShouldBindJSON(&callbackReq); err != nil {
+		errorCol.Add(err, zerolog.WarnLevel, true, nil, "job callback request has improper format")
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error":      "Invalid request",
+			"request_id": appctx.RequestID(c.Request.Context()),
+		})
+		return
+	}
+
+	contextJob, ok := c.Get("job")
+	if !ok {
+		errorCol.Add(nil, zerolog.ErrorLevel, false, nil, "job object not found in context")
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error":      "Internal server error",
+			"request_id": appctx.RequestID(c.Request.Context()),
+		})
+		return
+	}
+	job, _ := contextJob.(model.Job)
+
+	updatedJob, err := h.jobService.UpdateFromCallback(c.Request.Context(), &job, callbackReq)
+	if err != nil {
+		errorCol.Add(err, zerolog.ErrorLevel, false, map[string]any{"job": updatedJob}, "failed to update job from callback")
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error":      "Internal server error",
+			"request_id": appctx.RequestID(c.Request.Context()),
+		})
+		return
+	}
+
+	// TODO: Create reservation
+	// TODO: Send notification
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Callback request accepted successfully",
+	})
+	c.Set("message", "received and handled job callback")
 }
