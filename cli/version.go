9ee606672a774aa4752ddd44e16130ac11858171
Daylam Tayari <git@tayari.gg>
2026-02-11 12:13:07 -0600

    Moved cli version retrieval logic to func

--- cli/version.go
+++ cli/version.go
@@ -11,14 +11,24 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Retrieve program version",
 	Run: func(cmd *cobra.Command, args []string) {
-		if info, ok := debug.ReadBuildInfo(); ok {
-			version := info.Main.Version
-			if version == "" || version == "(devel)" {
-				version = "dev"
-			}
-			fmt.Printf("version %s", version)
-		} else {
-			fmt.Print("unknown")
+		version := getVersion()
+		if version != "unknown" {
+			version = "version " + version
 		}
+
+		fmt.Print(version)
 	},
+}
+
+// Retrieve the version of the CLI
+func getVersion() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		version := info.Main.Version
+		if version == "" || version == "(devel)" {
+			version = "dev"
+		}
+		return version
+	} else {
+		return "unknown"
+	}
 }
