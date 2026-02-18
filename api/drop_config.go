c5dc424f6ac595b7125f51825d463e4d7db26f29
Daylam Tayari <git@tayari.gg>
2026-02-17 21:59:10 -0600

    Added drop config api methods

--- api/drop_config.go
+++ api/drop_config.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -17,4 +18,51 @@ type DropConfig struct {
 	Confidence    int16          `json:"confidence"`
 
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type dropConfigCreateRequest struct {
+	Restaurant    uuid.UUID
+	DaysInAdvance int16
+	DropTime      string
+}
+
+// Retrieves all drop configs for a specified restaurant
+// If none exist, an empty slice is returned
+func (c *Client) GetDropConfigs(restaurantId uuid.UUID) ([]DropConfig, error) {
+	reqUrl := c.host + "/api/drop-config?restaurant=" + restaurantId.String()
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var dropConfigs []DropConfig
+	err = c.Do(req, &dropConfigs)
+	if err != nil {
+		return nil, err
+	}
+
+	return dropConfigs, nil
+}
+
+// Creates a new drop config and returns the drop config object and an error that is nil if successful
+// NOTE: The drop time value must be in HH:mm format otherwise the server will return an error
+func (c *Client) CreateDropConfig(restaurantId uuid.UUID, daysInAdvance int16, dropTime string) (DropConfig, error) {
+	reqUrl := c.host + "/api/drop-config"
+	dropConfigCreateReq := dropConfigCreateRequest{
+		Restaurant:    restaurantId,
+		DaysInAdvance: daysInAdvance,
+		DropTime:      dropTime,
+	}
+	req, err := c.NewJsonRequest(http.MethodPost, reqUrl, dropConfigCreateReq)
+	if err != nil {
+		return DropConfig{}, err
+	}
+
+	var dropConfig DropConfig
+	err = c.Do(req, &dropConfig)
+	if err != nil {
+		return DropConfig{}, err
+	}
+
+	return dropConfig, nil
 }
