c4c9de7a91689cb1fca5f2dd35388db7d67b5a67
Daylam Tayari <git@tayari.gg>
2026-04-05 19:28:59 -0500

    server: Added token renewal interval to config

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -8,15 +8,16 @@ import (
 )
 
 type Config struct {
-	Environment    Environment            `json:"environment" default:"dev"`
-	LogLevel       zerolog.Level          `json:"log_level" default:"info"`
-	Server         Server                 `json:"server"`
-	Database       Database               `json:"database"`
-	Auth           Auth                   `json:"auth"`
-	TokenStorePath string                 `json:"token_store_path" default:"./data/token_store"`
-	Cloud          Cloud                  `json:"cloud"`
-	Notification   []NotificationProvider `json:"notification"`
-	DefaultAdmin   User                   `json:"default_admin"`
+	Environment          Environment            `json:"environment" default:"dev"`
+	LogLevel             zerolog.Level          `json:"log_level" default:"info"`
+	Server               Server                 `json:"server"`
+	Database             Database               `json:"database"`
+	Auth                 Auth                   `json:"auth"`
+	TokenStorePath       string                 `json:"token_store_path" default:"./data/token_store"`
+	Cloud                Cloud                  `json:"cloud"`
+	Notification         []NotificationProvider `json:"notification"`
+	DefaultAdmin         User                   `json:"default_admin"`
+	TokenRenewalInterval Duration               `json:"token_renewal_interval" default:"24h"`
 }
 
 type Environment string
